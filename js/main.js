7169c6c95bf2bb78069c601985260e9e3bbd6b10
Critical <t01089416422@gmail.com>
2021-10-29 09:02:34 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$( 'a.start_button' ).click( function() {
+    $( 'div.main_container' ).fadeOut(850);
+  } );
+
+
+
+$(document).ready(function(){
+  $("a.start_button").click(function(){
+    $("div.main_screen").fadeIn(400);
+  })
+});
